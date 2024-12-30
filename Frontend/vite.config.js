d0da6826238deef4aa7f7befbadd0514fdaf9a88
Rohit-Singh28 <rohitsingh450718@gmail.com>
2024-12-30 16:42:32 +0530

    fix : fix changes in config file

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/api':'https://e-commerce-beta-self-10.vercel.app/',
+      '/api' : 'https://e-commerce-beta-self-10.vercel.app',
     }
   },
   plugins: [react()],
