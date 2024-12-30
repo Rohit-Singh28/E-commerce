27d137aa55bc3e7aa9c2f4435468f1f202011110
Rohit-Singh28 <rohitsingh450718@gmail.com>
2024-12-30 17:22:59 +0530

    fix:Added cors functionality

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,10 @@ app.use(express.json());
 app.use(cookieParser());
 // app.use(methodOverride('_method'));
 
-app.use(cors());
+app.use(cors({
+    origin : process.env.FRONTEND_URL,
+    credentials : true
+}))
 
 app.get("/",(req,res) => {
     res.send("home")
